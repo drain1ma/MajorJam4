bc4f2e943fd0273c2cd94c98d13623edb93c270f
drain1ma <drain1ma@cmich.edu>
2021-08-10 22:49:32 -0400

    Added lights/player/background/tileset

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed; 
+    private Rigidbody2D rb;
+    private SpriteRenderer sr; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if (horizontal < 0)
+            sr.flipX = true;
+        else
+            sr.flipX = false; 
+
+
     }
 }

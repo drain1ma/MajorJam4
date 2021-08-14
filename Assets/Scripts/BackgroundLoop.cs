271f52f0d8a27b82090aa608dfe8cb45dc47cc5c
drain1ma <drain1ma@cmich.edu>
2021-08-13 22:26:53 -0400

    updated BackgroundLoop script

--- Assets/Scripts/BackgroundLoop.cs
+++ Assets/Scripts/BackgroundLoop.cs
@@ -7,63 +7,57 @@ public class BackgroundLoop : MonoBehaviour
     public GameObject[] levels;
     private Camera mainCamera;
     private Vector2 screenBounds;
-    public float choke; 
-    // Start is called before the first frame update
+    public float choke;
+
     void Start()
     {
         mainCamera = gameObject.GetComponent<Camera>();
         screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
         foreach (GameObject obj in levels)
-            loadChildObjects(obj); 
+        {
+            loadChildObjects(obj);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void loadChildObjects(GameObject obj)
+    void loadChildObjects(GameObject obj)
     {
         float objectWidth = obj.GetComponent<SpriteRenderer>().bounds.size.x - choke;
-        int childNeeds = (int)Mathf.Ceil(screenBounds.x * 2 / objectWidth);
-        GameObject clone = Instantiate(obj) as GameObject; 
-        for (int i = 0; i < childNeeds; i++)
+        int childsNeeded = (int)Mathf.Ceil(screenBounds.x * 2 / objectWidth);
+        GameObject clone = Instantiate(obj) as GameObject;
+        for (int i = 0; i <= childsNeeded; i++)
         {
             GameObject c = Instantiate(clone) as GameObject;
             c.transform.SetParent(obj.transform);
             c.transform.position = new Vector3(objectWidth * i, obj.transform.position.y, obj.transform.position.z);
-            c.name = obj.name + i; 
+            c.name = obj.name + i;
         }
         Destroy(clone);
-        Destroy(obj.GetComponent<SpriteRenderer>()); 
+        Destroy(obj.GetComponent<SpriteRenderer>());
     }
     void repositionChildObjects(GameObject obj)
     {
-        Transform[] children = obj.GetComponentsInChildren<Transform>(); 
+        Transform[] children = obj.GetComponentsInChildren<Transform>();
         if (children.Length > 1)
         {
             GameObject firstChild = children[1].gameObject;
             GameObject lastChild = children[children.Length - 1].gameObject;
             float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x - choke;
-            if (transform.position.x + screenBounds.x > lastChild.transform.position.x + halfObjectWidth)
+            if (transform.position.x + screenBounds.x > lastChild.transform.position.x)
             {
                 firstChild.transform.SetAsLastSibling();
-                firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfObjectWidth * 2, lastChild.transform.position.y, lastChild.transform.position.z); 
-
+                firstChild.transform.position = new Vector3(lastChild.transform.position.x * 2, lastChild.transform.position.y, lastChild.transform.position.z);
             }
-            else if (transform.position.x - screenBounds.x < firstChild.transform.position.x - halfObjectWidth)
+            else if (transform.position.x - screenBounds.x < firstChild.transform.position.x)
             {
                 lastChild.transform.SetAsFirstSibling();
-                lastChild.transform.position = new Vector3(firstChild.transform.position.x - halfObjectWidth, firstChild.transform.position.y, firstChild.transform.position.z); 
+                lastChild.transform.position = new Vector3(firstChild.transform.position.x * 2, firstChild.transform.position.y, firstChild.transform.position.z);
             }
         }
     }
-    private void LateUpdate()
+    void LateUpdate()
     {
-        foreach(GameObject obj in levels)
+        foreach (GameObject obj in levels)
         {
-            repositionChildObjects(obj); 
+            repositionChildObjects(obj);
         }
     }
 }
